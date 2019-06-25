b2e7713a3f6c41e9c38a735051fb266b14673a60
dldonofrio <dandono97@gmail.com>
2019-06-25 10:16:15 -0400

    v1.0

---
+++ instagram_request.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+def instagram_pull(username, number_of_posts):
+	insta_posts = []
+	r = requests.get('https://instagram.com/' + username + '?__a=1')
+	if r.status_code == requests.codes.ok:
+		for x in range(0, number_of_posts):
+			insta_JSON = r.json()
+			post = {
+				'url_img': insta_JSON['graphql']['user']['edge_owner_to_timeline_media']['edges'][x]['node']['display_url'],
+				'url_post': 'https://www.instagram.com/p/' + insta_JSON['graphql']['user']['edge_owner_to_timeline_media']['edges'][x]['node']['shortcode'],
+				'text': insta_JSON['graphql']['user']['edge_owner_to_timeline_media']['edges'][x]['node']['edge_media_to_caption']['edges'][0]['node']['text'],
+				'likes': insta_JSON['graphql']['user']['edge_owner_to_timeline_media']['edges'][x]['node']['edge_liked_by']['count']
+			}
+			insta_posts.append(post)	
+	else:
+		print(r.status_code)
+		r.raise_for_status()
+	print(insta_posts)
+	return insta_posts
+
+instagram_pull('doner_agency', 3)
